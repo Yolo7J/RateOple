9bcc7cdc94f3d174b0988504e3a8a1156fc8f4e7
Yolo7J <potato7morkov@gmail.com>
2026-02-11 09:32:53 +0200

    Added Group Entities

---
+++ backend/RateOple.Infrastructure/Data/Models/Group.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using RateOple.Constants.Enums;
+
+namespace RateOple.Infrastructure.Data.Models;
+
+public class Group
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    [Required]
+    public GroupVisibility Visibility { get; set; } = GroupVisibility.Public;
+
+    [Required]
+    public Guid OwnerId { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation Properties
+    public User Owner { get; set; } = null!;
+    public ICollection<GroupMembership> Members { get; set; } = new List<GroupMembership>();
+    public ICollection<GroupPost> Posts { get; set; } = new List<GroupPost>();
+    public ICollection<GroupMedia> MediaLinks { get; set; } = new List<GroupMedia>();
+}
