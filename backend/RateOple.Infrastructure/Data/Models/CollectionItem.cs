251a40d07f0d4b5e672aeeaf26373a91ccb50be3
Yolo7J <potato7morkov@gmail.com>
2026-02-11 09:30:47 +0200

    Added Collection and CollectionItem entities

---
+++ backend/RateOple.Infrastructure/Data/Models/CollectionItem.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RateOple.Infrastructure.Data.Models;
+
+public class CollectionItem
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    public Guid CollectionId { get; set; }
+
+    [Required]
+    public Guid MediaId { get; set; }
+
+    public DateTime AddedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation Properties
+    public Collection Collection { get; set; } = null!;
+    public Media Media { get; set; } = null!;
+}
