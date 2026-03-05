ba0e59ab19ed762f598336b4c8a55b2b8002dce4
Yolo7J <potato7morkov@gmail.com>
2026-03-05 10:03:09 +0200

    Updated TvSeries Entity

---
+++ backend/RateOple.Infrastructure/Migrations/20260305080133_CreateTvSeriesAsync.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RateOple.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTvSeriesAsync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
