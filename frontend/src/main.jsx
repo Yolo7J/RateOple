692562d6b60e62efa37fa11a9ad4c4ca9b33459d
Yolo7J <potato7morkov@gmail.com>
2026-03-08 16:45:20 +0200

    Move global layout components into shared/components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { AuthProvider } from './context/AuthContext'
 import { MediaCartProvider } from './context/MediaCartContext'
-import Header from './components/layout/Header/Header.jsx'
-import Footer from './components/layout/Footer/Footer.jsx'
+import Header from './shared/components/Header/Header.jsx'
+import Footer from './shared/components/Footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
